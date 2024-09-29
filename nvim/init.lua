d7a6bfcaebe550d769bab11fb314337e8acd9e7c
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-09-30 00:32:41 +0530

    [nvim] setup DAP

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,8 +8,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -20,16 +18,19 @@ vim.opt.smartcase = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.termguicolors = true
-
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.guicursor = ''
 vim.opt.inccommand = 'split'
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
 
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 
-vim.api.nvim_create_autocmd({'TermEnter', 'TermLeave'}, {
-  command = 'set invnu invrnu',
-})
+vim.api.nvim_create_autocmd({ 'TermEnter', 'TermLeave' }, { command = 'set invnu invrnu' })
+
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -44,44 +45,66 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', 'cr', vim.lsp.buf.rename)
 vim.keymap.set({ 'n', 'v' }, 'ga', vim.lsp.buf.code_action)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>=', vim.lsp.buf.format)
-
-vim.keymap.set('n', '-', '<cmd>Oil<CR>')
-vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<CR>')
-vim.keymap.set('n', '<leader>g', '<cmd>Git<CR>')
-vim.keymap.set('n', '<leader>j', '<cmd>Telescope jumplist<CR>')
-vim.keymap.set('n', '<leader><leader>', '<cmd>Telescope find_files<CR>')
-vim.keymap.set('n', '<leader>*', '<cmd>Telescope grep_string<CR>')
-vim.keymap.set('n', '<leader>/', '<cmd>Telescope live_grep<CR>')
-vim.keymap.set('n', "<leader>'", '<cmd>Telescope resume<CR>')
-vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<CR>')
-vim.keymap.set('n', '<leader>,', '<cmd>Telescope buffers<CR>')
-vim.keymap.set('n', 'cx', '<cmd>Telescope diagnostics<CR>')
-vim.keymap.set('n', 'cs', '<cmd>Telescope lsp_document_symbols<CR>')
-vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>')
-vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>')
-vim.keymap.set('n', 'gI', '<cmd>Telescope lsp_implementations<CR>')
 
 local spec = {
   'tpope/vim-sleuth',
-  'tpope/vim-fugitive',
-  'mbbill/undotree',
+  'tpope/vim-surround',
   'nvim-tree/nvim-web-devicons',
   'neovim/nvim-lspconfig',
 
-  { 'numToStr/Comment.nvim',   opts = {} },
-  { 'lewis6991/gitsigns.nvim', opts = {} },
-  { 'dundalek/lazy-lsp.nvim',  opts = {}, },
-  { 'j-hui/fidget.nvim',       opts = {} },
-  { 'stevearc/oil.nvim',       opts = { skip_confirm_for_simple_edits = true } },
+  { 'williamboman/mason.nvim',     opts = {} },
+  { 'numToStr/Comment.nvim',       opts = {} },
+  { 'lewis6991/gitsigns.nvim',     opts = {} },
+  { 'j-hui/fidget.nvim',           opts = {} },
+  { 'echasnovski/mini.statusline', opts = {} },
+  { 'echasnovski/mini.ai',         opts = {} },
+
+  { 'tpope/vim-fugitive',          keys = { { '<leader>g', '<cmd>Git<CR>' } } },
+  { 'mbbill/undotree',             keys = { { '<leader>u', '<cmd>UndotreeToggle<CR>' } } },
+
+  {
+    'stevearc/oil.nvim',
+    lazy = false,
+    opts = {
+      keymaps = {
+        ['<C-v>'] = 'actions.select_vsplit',
+        ['<C-s>'] = 'actions.select_split',
+      },
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+    },
+    keys = { { '-', '<cmd>Oil<CR>' } }
+  },
+
+  {
+    'williamboman/mason-lspconfig.nvim',
+    opts = { handlers = { function(server) require('lspconfig')[server].setup({}) end } }
+  },
+
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = {
+      ensure_installed = {
+        'lua-language-server', 'bash-language-server', 'gopls', 'zls', 'pyright', 'rust-analyzer', 'clangd', 'elixir-ls',
+        'emmet-ls', 'eslint', 'cpptools',
+      },
+      auto_update = true,
+    }
+  },
+
+  {
+    'stevearc/conform.nvim',
+    opts = { format_on_save = { timeout_ms = 500, lsp_format = 'fallback' } },
+    keys = {
+      { '<leader>=', function() require('conform').format({ async = true, lsp_format = 'fallback' }) end },
+    }
+  },
 
   {
     'folke/lazydev.nvim',
     ft = 'lua',
     dependencies = { { 'Bilal2453/luvit-meta', lazy = true } },
-    opts = {
-      library = { { path = 'luvit-meta/library', words = { 'vim%.uv' } } },
-    },
+    opts = { library = { { path = 'luvit-meta/library', words = { 'vim%.uv' } } } },
   },
 
   {
@@ -94,7 +117,24 @@ local spec = {
     config = function()
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
-    end
+    end,
+    keys = function()
+      local telescope = require('telescope.builtin')
+      return {
+        { '<leader><leader>', telescope.find_files },
+        { '<leader>j',        telescope.jumplist },
+        { '<leader>*',        telescope.grep_string },
+        { '<leader>/',        telescope.live_grep },
+        { "<leader>'",        telescope.resume },
+        { '<leader>?',        telescope.oldfiles },
+        { '<leader>,',        telescope.buffers },
+        { 'cx',               telescope.diagnostics },
+        { 'cs',               telescope.lsp_document_symbols },
+        { 'gd',               telescope.lsp_definitions },
+        { 'gr',               telescope.lsp_references },
+        { 'gI',               telescope.lsp_implementations },
+      }
+    end,
   },
 
   {
@@ -133,17 +173,14 @@ local spec = {
         dependencies = {
           {
             'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
+            config = function() require('luasnip.loaders.from_vscode').lazy_load() end,
           },
         },
       },
     },
     config = function()
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
 
       cmp.setup {
         snippet = {
@@ -190,6 +227,82 @@ local spec = {
         matching = { disallow_symbol_nonprefix_matching = false }
       })
     end,
+  },
+
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      { 'rcarriga/nvim-dap-ui',         opts = {} },
+      'nvim-neotest/nvim-nio',
+      { 'leoluz/nvim-dap-go',           opts = {} },
+      { 'jay-babu/mason-nvim-dap.nvim', opts = {} },
+    },
+    config = function()
+      local dap                                          = require('dap')
+
+      local elixir_ls_debugger                           = vim.fn.exepath('elixir-ls-debugger')
+      dap.adapters.mix_task                              = { type = 'executable', command = elixir_ls_debugger }
+      dap.configurations.elixir                          = {
+        {
+          type = 'mix_task',
+          name = 'phoenix server',
+          task = 'phx.server',
+          request = 'launch',
+          projectDir = '${workspaceFolder}',
+          exitAfterTaskReturns = false,
+          debugAutoInterpretAllModules = false,
+        },
+      }
+
+      local cpptools                                     = vim.fn.exepath('OpenDebugAD7')
+      dap.adapters.cppdbg                                = { id = 'cppdbg', type = 'executable', command = cpptools }
+      dap.configurations.cpp                             = {
+        {
+          name = "Launch file",
+          type = "cppdbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopAtEntry = true,
+        },
+        {
+          name = 'Attach to gdbserver :1234',
+          type = 'cppdbg',
+          request = 'launch',
+          MIMode = 'gdb',
+          miDebuggerServerAddress = 'localhost:1234',
+          miDebuggerPath = '/usr/bin/gdb',
+          cwd = '${workspaceFolder}',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+        },
+      }
+
+      local dapui                                        = require('dapui')
+      dap.listeners.before.attach.dapui_config           = function() dapui.open() end
+      dap.listeners.before.launch.dapui_config           = function() dapui.open() end
+      dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+      dap.listeners.before.event_exited.dapui_config     = function() dapui.close() end
+    end,
+
+    keys = function()
+      local dap, dapui = require('dap'), require('dapui')
+      return {
+        { '<leader>dc', dap.continue },
+        { '<leader>db', dap.toggle_breakpoint },
+        { "<leader>dC", function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end },
+        { '<leader>di', dap.step_into },
+        { '<leader>do', dap.step_out },
+        { '<leader>dO', dap.step_over },
+        { '<leader>dB', dap.step_back },
+        { '<leader>dr', dap.restart },
+        { '<leader>du', dapui.toggle },
+        { '<leader>de', function() dapui.eval(nil, { enter = true }) end },
+      }
+    end
   },
 }
 
